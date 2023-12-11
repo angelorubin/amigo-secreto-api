a8b9cd6527019d54b7ee6f41ab8c8a67b15b1e1d
angelorubin <angelorubin@gmail.com>
2023-12-11 20:23:02 -0300

    Organizando as rotas

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -4,14 +4,15 @@ import * as events from "../controllers/events";
 
 const router = Router();
 
+// admin/login routes
+router.post("/login", auth.login);
 router.post("/ping", auth.validate, (req, res) => {
   return res.json({ pong: true, admin: true });
 });
 
+// admin/events routes
 router.get("/events", auth.validate, events.getAll);
 router.get("/events/:id", auth.validate, events.getEventById);
 router.post("/events", auth.validate, events.addEvent);
-
-router.post("/login", auth.login);
 
 export default router;
