e9add09862cebc3f36938a1dd122c02a4ef340f1
angelorubin <angelorubin@gmail.com>
2023-12-11 20:20:50 -0300

    Validando password

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,6 @@
-import { RequestHandler } from "express";
-import { z, SafeParseReturnType } from "zod";
-import * as auth from "../services/auth";
+import { RequestHandler } from 'express';
+import { z, SafeParseReturnType } from 'zod';
+import * as auth from '../services/auth';
 
 interface IData {
   email: string;
@@ -23,7 +23,11 @@ export const login: RequestHandler = (req, res) => {
   console.log(validation.success);
 
   if (!validation.success) {
-    res.json({ message: "Dados inválidos" });
+    res.json({ message: 'Dados inválidos' });
+  }
+
+  if (!auth.validatePassword(req.body.password)) {
+    return res.status(403).json({ error: 'Acesso negado.' })
   }
 
   res.json({ token: auth.createToken() });
@@ -33,11 +37,11 @@ export const login: RequestHandler = (req, res) => {
   const validation: ILogin = loginSchema.parse(req.body);
 
   if (!validation.success) {
-    res.json({ error: "Login com dados inválidos." })
+    res.json({ error: 'Login com dados inválidos.' })
   }
 
   if (!auth.validatePassword(req.body.password)) {
-    return res.status(403).json({ error: "Acesso negado." })
+    return res.status(403).json({ error: 'Acesso negado.' })
   }
 
   res.json({ token: auth.createToken() })
@@ -46,13 +50,13 @@ export const login: RequestHandler = (req, res) => {
 
 export const validate: RequestHandler = (req, res, next) => {
   if (!req.headers.authorization) {
-    return res.status(403).json({ error: "Acesso negado" });
+    return res.status(403).json({ error: 'Acesso negado' });
   }
 
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization.split(' ')[1];
 
   if (!auth.validateToken(token)) {
-    return res.status(403).json({ error: "Acesso negado" });
+    return res.status(403).json({ error: 'Acesso negado' });
   }
 
   next();
