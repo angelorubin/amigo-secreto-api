068fe55492e3aa8771cb963a8c06d86248b3cdbd
angelorubin <angelorubin@gmail.com>
2023-12-13 20:33:45 -0300

    Adicionando endpoint groups nas rotas

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,20 +1,25 @@
 import { Router } from "express";
 import * as auth from "../controllers/auth";
 import * as events from "../controllers/events";
+import * as groups from "../controllers/groups"
 
 const router = Router();
 
-// admin/login routes
+// admin/login
 router.post("/login", auth.login);
 router.post("/ping", (req, res) => {
   return res.json({ pong: true, admin: true });
 });
 
-// admin/events routes
+// admin/events
 router.get("/events", auth.validate, events.getEvents);
 router.get("/events/:id", auth.validate, events.getEventById);
 router.post("/events", auth.validate, events.createEvent);
 router.put("/events/:id", auth.validate, events.updateEvent)
 router.delete("/events/:id", auth.validate, events.destroyEvent)
+
+// admin/events/groups
+router.get("/events/:id_event/groups", auth.validate, groups.getGroups)
+router.get("/events/:id_event/groups/:id", auth.validate, groups.getGroupById)
 
 export default router;
