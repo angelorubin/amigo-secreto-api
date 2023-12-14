e2169ae1cd6a8229af044fed60b1b966094792b9
angelorubin <angelorubin@gmail.com>
2023-12-14 20:18:49 -0300

    Criando events/groups services

--- src/services/groups.ts
+++ src/services/groups.ts
@@ -1,21 +1,21 @@
-import { PrismaClient } from "@prisma/client";
-
+import { Prisma, PrismaClient } from "@prisma/client";
+import * as events from '../services/events'
 const prisma = new PrismaClient();
 
 export const getGroups = async (id_event: number) => {
   try {
-    return await prisma.eventGroup.findFirst({ where: { id_event } });
+    return await prisma.eventGroup.findMany({ where: { id_event } });
   } catch (error) {
     return false;
   }
 };
 
-type getGroupByIdFilters = {
+type GetGroupByIdFilters = {
   id: number
   id_event?: number
 }
 
-export const getGroupById = async (filters: getGroupByIdFilters) => {
+export const getGroupById = async (filters: GetGroupByIdFilters) => {
   try {
     return await prisma.eventGroup.findFirst({
       where: {
@@ -27,3 +27,35 @@ export const getGroupById = async (filters: getGroupByIdFilters) => {
     return false;
   }
 };
+
+type CreateGroupData = Prisma.Args<typeof prisma.eventGroup, 'create'>['data']
+
+export const createGroup = async (data: CreateGroupData) => {
+  try {
+    if (!data.id_event) {
+      return false
+    }
+
+    const eventItem = await events.getEventById(data.id_event)
+    if (!eventItem) return false
+
+    return await prisma.eventGroup.create({ data })
+
+  } catch (error) {
+    return false;
+  }
+};
+
+type UpdateFilters = { id: number; id_event: number; }
+type UpdateData = Prisma.Args<typeof prisma.eventGroup, 'update'>['data']
+
+export const updateGroup = async (filters: UpdateFilters, data: UpdateData) => {
+  try {
+    return await prisma.eventGroup.updateMany({
+      where: filters,
+      data: { name: data.name }
+    })
+  } catch (error) {
+    return false
+  }
+}
