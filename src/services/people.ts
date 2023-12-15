459cf4a4d35429843248c3b00471c14a20a0a51a
angelorubin <angelorubin@gmail.com>
2023-12-15 19:45:58 -0300

    Implementando people service

--- src/services/people.ts
+++ src/services/people.ts
@@ -1,12 +1,12 @@
 import { PrismaClient, Prisma } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-type RetrieveAllFilters = { id_event: number; id_group: number; }
+type RetrieveAllFilters = { id_event: number; id_group: number };
 export const retrieveAll = async (filters: RetrieveAllFilters) => {
   try {
-    return await prisma.eventPeople.findMany({ where: filters })
+    return await prisma.eventPeople.findMany({ where: filters });
   } catch (error) {
-
+    return false;
   }
-}
+};
