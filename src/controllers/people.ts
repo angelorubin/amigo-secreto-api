b7bbfe34c53ac6af366f200f57d6262387c511b3
angelorubin <angelorubin@gmail.com>
2023-12-15 19:43:56 -0300

    Implementando controller people retrieveAll

--- src/controllers/people.ts
+++ src/controllers/people.ts
@@ -3,11 +3,16 @@ import * as people from "../services/people";
 import { z } from "zod";
 
 export const retrieveAll: RequestHandler = async (req, res) => {
-  const { id_event, id_group } = req.params
+  const { id_event, id_group } = req.params;
 
   const items = await people.retrieveAll({
-    id_event: parseInt(id_event)
-  })
+    id_event: parseInt(id_event),
+    id_group: parseInt(id_group),
+  });
 
-  res.json({ error: "Ocorreu um erro" });
+  if (items) {
+    return res.json({ items });
+  }
+
+  return res.json({ error: "Ocorreu um erro" });
 };
