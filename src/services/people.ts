0999086ae760663168af2b072361f6811caebfe1
angelorubin <angelorubin@gmail.com>
2023-12-16 15:09:03 -0300

    Implementando serviço para adição de pessoa

--- src/services/people.ts
+++ src/services/people.ts
@@ -13,13 +13,26 @@ export const retrieveAll = async (filters: RetrieveAllFilters) => {
 
 type RetrievePersonFilters = {
   id_event: number;
-  id_group?: number;
+  id_group: number;
   id?: number;
-  cpf?: number;
+  cpf?: string;
 };
 export const retrievePerson = async (filters: RetrievePersonFilters) => {
   try {
+    if (!filters.id && filters.cpf) {
+      return false
+    }
+    return await prisma.eventPeople.findFirst({ where: filters })
   } catch (error) {
     return false;
   }
 };
+
+type CreatePersonFilters = {}
+export const createPerson = async (data: object, filters: CreatePersonFilters) => {
+  try {
+    return { filters, data }
+  } catch (error) {
+    return false
+  }
+}
