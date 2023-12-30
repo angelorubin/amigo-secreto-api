21ff425c17a13dfde62dd867ec4cda21232b7b73
angelorubin <angelorubin@gmail.com>
2023-12-30 15:03:47 -0300

    Adicionando rota de teste no site

--- src/routes/site.ts
+++ src/routes/site.ts
@@ -2,10 +2,13 @@ import { Router } from "express"
 import * as auth from "../controllers/auth"
 import * as events from '../controllers/events'
 import * as people from '../controllers/people'
+import { retrieveEvents } from "../services/events"
 
 const router = Router()
 
 // events routes
+router.get('/', (req, res) => res.json({ test: 'OK' }))
+router.get('/events', retrieveEvents)
 router.get('/events/:id', events.retrieveEvent)
 router.get('/events/:id_event/search', people.searchPerson)
 
